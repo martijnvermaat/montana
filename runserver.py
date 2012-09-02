dad4fd74718ef950106d65dc72c2c11f1429d869
Martijn Vermaat <martijn@vermaat.name>
2012-09-02 19:37:01 +0200

    Comments

--- runserver.py
+++ runserver.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-# https://github.com/mitsuhiko/flask-sqlalchemy/pull/81
+
+# Using an in-memory SQLite database should only be done with a static
+# connection pool such that all threads share the same connection.
+#
+# This is implemented in this pull request:
+#     https://github.com/mitsuhiko/flask-sqlalchemy/pull/81
+#
+# Until it is merged, we monkey patch Flask-SQLAlchemy.
 from sqlalchemy.pool import StaticPool
 from flask.ext.sqlalchemy import SQLAlchemy
 _apply_driver_hacks = SQLAlchemy.apply_driver_hacks
@@ -19,7 +26,10 @@ SQLAlchemy.apply_driver_hacks = apply_driver_hacks
 from sqlalchemy.engine.url import make_url
 from montana import create_app, db
 
+
 app = create_app()
+
+# If we have an in-memory database, setup the schema first.
 info = make_url(app.config['SQLALCHEMY_DATABASE_URI'])
 if info.drivername == 'sqlite' and info.database in (None, '', ':memory:'):
     print 'Setting up in-memory database...'
@@ -27,4 +37,5 @@ if info.drivername == 'sqlite' and info.database in (None, '', ':memory:'):
         db.create_all()
         db.session.commit()
     print 'Done setting up in-memory database'
-app.run(debug=True)
+
+app.run()
